6edf4137ef0a0309630d5d0780cb5bc0d444787d
May <89690968+Meroy-Cheng@users.noreply.github.com>
2022-12-12 11:38:32 +0800

    Update CalculatorTest.java

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -5,7 +5,19 @@ public class CalculatorTest {
   @Test
   public void addTest() {
     Calculator calculator = new Calculator();
-    int sum = calculator.add(1, 5);
-    assertEquals(6, sum);
+    int ans = calculator.add(1, 5);
+    assertEquals(6, ans);
+  }
+  @Test
+  public void addTest() {
+    Calculator calculator = new Calculator();
+    int ans = calculator.minus(10, 5);
+    assertEquals(5, ans);
+  }
+  @Test
+  public void addTest() {
+    Calculator calculator = new Calculator();
+    int ans = calculator.multiplication(3, 5);
+    assertEquals(15, ans);
   }
 }

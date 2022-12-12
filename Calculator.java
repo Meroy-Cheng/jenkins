bba0d66b36406c1884356ac4020b5f6aee769287
May <89690968+Meroy-Cheng@users.noreply.github.com>
2022-12-12 11:36:59 +0800

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -2,4 +2,10 @@ public class Calculator {
   public int add(int x, int y) {
     return x + y;
   }
+  public int minus(int x, int y) {
+    return x - y;
+  }
+  public int multiplication(int x, int y) {
+    return x * y;
+  }
 }
